b3ad7f1961fbc618b7032f8d0d3592babeb3dbfb
Khanh Le <lekhanhsone@gmail.com>
2024-10-08 16:08:43 -0600

    New update #3 - Added more components to Account Screen

---
+++ components/ListItem.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { View, StyleSheet, Image, TouchableHighlight } from "react-native";
+import AppText from "./AppText";
+import colors from "../app/config/colors";
+
+function ListItem({ title, description, image, IconComponent, onPress }) {
+  return (
+    <TouchableHighlight underlayColor={colors.screenWhite} onPress={onPress}>
+      <View style={styles.container}>
+        {IconComponent}
+        {image && <Image source={image} style={styles.image} />}
+        <View style={styles.detailedContainer}>
+          <AppText style={styles.title}>{title}</AppText>
+          {description && (
+            <AppText style={styles.description}>{description}</AppText>
+          )}
+        </View>
+      </View>
+    </TouchableHighlight>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    padding: 15,
+    backgroundColor: colors.white,
+  },
+  description: {
+    color: colors.medium,
+  },
+  detailedContainer: {
+    marginLeft: 10,
+    justifyContent: "center",
+  },
+  image: {
+    width: 70,
+    height: 70,
+    borderRadius: 35,
+  },
+  title: {
+    fontWeight: "500",
+  },
+});
+
+export default ListItem;
---
+++ components/ListItemSeperator.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import colors from '../app/config/colors';
+
+function ListItemSeperator(props) {
+    return (
+        <View style={styles.seperator}></View>
+    );
+}
+const styles = StyleSheet.create({
+    seperator:{
+        height: 1,
+        width: '100%',
+        backgroundColor: colors.lightGrey,
+    }
+})
+
+export default ListItemSeperator;
